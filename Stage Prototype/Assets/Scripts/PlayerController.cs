ac794ec57066bc93f8e578bfd48ef9bb38b5fd7a
Corbin Maze <corbinmaze@gmail.com>
2022-11-09 21:17:07 -0500

    Added Point system
    Added point system (a little buggy but it works)

---
+++ Stage Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private int count;
+
+    public Text countText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScoreCount();
+        count = 0;
+    }
+
+    void SetScoreCount()
+    {
+        countText.text = "Fan Points: " + count.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Note"))
+        {
+         
+            count += 150;
+
+            SetScoreCount();
+        }
+
+    }
+}

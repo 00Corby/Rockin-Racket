304fae66d349bf5497779318f231299e00518bd5
Corbin Maze <corbinmaze@gmail.com>
2022-12-01 14:14:58 -0500

    Added UI and Auto player

---
+++ Stage Prototype/Assets/Scripts/AutoPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoPlayer : MonoBehaviour
+{
+
+    public Slider Vocals_slider;
+    public Slider Guitar_slider;
+    public Slider Bass_slider;
+    public Slider Drums_slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
